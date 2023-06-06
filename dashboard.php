afc5255e42f12e085b92d536dbbcd5a748c15efa
Breno <brenoeller@hotmail.com>
2023-06-06 19:22:14 -0300

    new - estrutura do dashboard

--- dashboard.php
+++ dashboard.php
@@ -2,11 +2,46 @@
 
     include_once("templates/header.php") 
 
-    ?>
+?>
 
-    <h1>dashboard</h1>
+    <div id="main-banner">
+        <h1>Faça seu pedido</h1>
+    </div>
+
+    <div id="main-container">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Selecione seu pedido:</h2>
+                    <form action="process/pizza.php" id="pizza-form" method="POST">
+                        <div class="form-group">
+                            <label for="borda">Borda:</label>
+                            <select name="borda" id="borda" class="form-control">
+                                <option value="">Selecione a borda da pizza</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="borda">Massa:</label>
+                            <select name="massa" id="massa" class="form-control">
+                                <option value="">Selecione a massa da pizza</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="borda">Sabores: (máximo 3)</label>
+                            <select multiple name="sabores[]" id="sabores" class="form-control">
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" class="btn btn-primary" value="Fazer pedido">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
 <?php
 
     include_once("templates/footer.php") 
 
-    ?>
+?>
