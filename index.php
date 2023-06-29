5bdd65ce8bd9e80702d12690e20ae408aa366b18
Breno <brenoeller@hotmail.com>
2023-06-29 14:51:21 -0300

    new - conexão com o banco de dados

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php
 
-    include_once("templates/header.php") 
+    include_once("templates/header.php");
+    include_once("process/pizza.php");
 
     ?>
 
-
+<body>
+    <?php include_once("dashboard.php"); ?>
+</body>
 <?php
 
     include_once("templates/footer.php") 
